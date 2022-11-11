f8db2080b53b0eb60ccb259c3c282e712ad4bd88
Chowchow <gOzaru@users.noreply.github.com>
2022-11-11 10:49:38 +0800

    Updated

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todo/new_todo_dialog.dart';
 import 'package:todo/todo.dart';
